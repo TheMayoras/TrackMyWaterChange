35fc4e5b853499bb7312dda2b46f8eba95426068
themayoras <ben.mayoras@gmail.com>
2020-05-29 13:18:46 -0400

    Removed testing data

--- src/main/resources/static/app/index/index.component.js
+++ src/main/resources/static/app/index/index.component.js
@@ -1,10 +1,6 @@
 "use strict";
 
-angular
-    .module("index")
-    .component("index", {
-        templateUrl: "app/index/index.template.html",
-        controller: function ($scope) {
-            $scope.data = "Ben";
-        },
+angular.module("index").component("index", {
+  templateUrl: "app/index/index.template.html",
+  controller: function () {},
 });
