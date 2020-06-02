c6a81ba6270676b0b7f139c79bf655f1a50fa894
themayoras <ben.mayoras@gmail.com>
2020-06-02 09:41:24 -0400

    Add null check to User.setEmail

--- src/main/java/themayoras/trackmywaterchange/entity/User.java
+++ src/main/java/themayoras/trackmywaterchange/entity/User.java
@@ -1,28 +1,16 @@
 package themayoras.trackmywaterchange.entity;
 
+import themayoras.trackmywaterchange.entity.validation.Email;
+import themayoras.trackmywaterchange.entity.validation.Username;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-import themayoras.trackmywaterchange.entity.validation.Email;
-import themayoras.trackmywaterchange.entity.validation.Username;
 
 @Entity
 @Table(name = "users")
@@ -67,31 +55,31 @@ public class User {
     private List<Tank> tanks;
 
     // the user's roles
-    @ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name = "users_user_roles", joinColumns = { @JoinColumn(name = "id_user") }, inverseJoinColumns = {
-	    @JoinColumn(name = "id_user_role") })
+            @JoinColumn(name = "id_user_role") })
     private Set<Role> roles;
 
     public void addTank(Tank tank) {
-	if (tanks == null) {
-	    tanks = new ArrayList<>();
-	}
+        if (tanks == null) {
+            tanks = new ArrayList<>();
+        }
 
-	tanks.add(tank);
+        tanks.add(tank);
     }
 
     public void addRole(Role role) {
-	if (roles == null) {
-	    roles = new HashSet<>();
-	}
+        if (roles == null) {
+            roles = new HashSet<>();
+        }
 
-	roles.add(role);
+        roles.add(role);
     }
 
     public void remove(Role role) {
-	if (roles != null) {
-	    roles.remove(role);
-	}
+        if (roles != null) {
+            roles.remove(role);
+        }
     }
 
     public User() {
@@ -99,123 +87,123 @@ public class User {
     }
 
     public User(String username) {
-	this.username = username;
+        this.username = username;
     }
 
     public User(String username, String email) {
-	this.username = username;
-	this.email = email;
+        this.username = username;
+        this.email = email;
     }
 
     public User(String username,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName) {
-	this.username = username;
-	this.firstName = firstName;
-	this.lastName = lastName;
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public User(String username,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName,
-	    @Email(message = "invalid email") String email) {
-	this.username = username;
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.setEmail(email);
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName,
+                @Email(message = "invalid email") String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.setEmail(email);
     }
 
     public User(String username,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
-	    @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName,
-	    @Email(message = "invalid email") String email, List<Tank> tanks) {
-	this.username = username;
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.setEmail(email);
-	this.tanks = tanks;
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String firstName,
+                @NotNull(message = "is required") @Pattern(regexp = "^[a-zA-Z]+$", message = "invalid name") String lastName,
+                @Email(message = "invalid email") String email, List<Tank> tanks) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.setEmail(email);
+        this.tanks = tanks;
     }
 
     public int getId() {
-	return id;
+        return id;
     }
 
     public void setId(int id) {
-	this.id = id;
+        this.id = id;
     }
 
     public String getUsername() {
-	return username;
+        return username;
     }
 
     public void setUsername(String username) {
-	this.username = username;
+        this.username = username;
     }
 
     public String getPassword() {
-	return this.password;
+        return this.password;
     }
 
     public void setPassword(String password) {
-	this.password = password;
+        this.password = password;
     }
 
     public String getFirstName() {
-	return firstName;
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
-	this.firstName = firstName;
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-	return lastName;
+        return lastName;
     }
 
     public void setLastName(String lastName) {
-	this.lastName = lastName;
+        this.lastName = lastName;
     }
 
     public String getEmail() {
-	return email;
+        return email;
     }
 
     public void setEmail(String email) {
-	if (email.trim().length() == 0) {
-	    email = null;
-	}
+        if (email != null && email.trim().length() == 0) {
+            email = null;
+        }
 
-	this.email = email;
+        this.email = email;
     }
 
     public List<Tank> getTanks() {
-	return tanks;
+        return tanks;
     }
 
     public void setTanks(List<Tank> tanks) {
-	this.tanks = tanks;
+        this.tanks = tanks;
     }
 
     public Set<Role> getRoles() {
-	return this.roles;
+        return this.roles;
     }
 
     public void setRoles(Set<Role> roles) {
-	this.roles = roles;
+        this.roles = roles;
     }
 
     @Override
     public String toString() {
-	return "User [id=" + this.id + ", username=" + this.username + ", password=" + this.password + ", firstName="
-		+ this.firstName + ", lastName=" + this.lastName + ", email=" + this.email + ", tanks=" + this.tanks
-		+ ", roles=" + this.roles + "]";
+        return "User [id=" + this.id + ", username=" + this.username + ", password=" + this.password + ", firstName="
+                       + this.firstName + ", lastName=" + this.lastName + ", email=" + this.email + ", tanks=" + this.tanks
+                       + ", roles=" + this.roles + "]";
     }
 
     public void remove(Tank tank) {
-	if (tanks != null) {
-	    tanks.remove(tank);
-	    tank.setUser(null);
-	}
+        if (tanks != null) {
+            tanks.remove(tank);
+            tank.setUser(null);
+        }
     }
 
 }
